3ef4d12f546942e4159f67952fcb6c6fb5ec6c7e
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2014-02-08 03:43:33 -0300

    Fixed layout

--- bluelytics/dolar_blue/models.py
+++ bluelytics/dolar_blue/models.py
@@ -12,11 +12,18 @@ class DolarBlue(models.Model):
     value_sell = models.DecimalField(decimal_places=6, max_digits=10)
     date = models.DateTimeField('fecha')
 
+    def _get_value_avg(self):
+       "Returns the average price"
+       return (self.value_buy + self.value_sell) / 2
+    value_avg = property(_get_value_avg)
+
+
     def json(self):
       arg = pytz.timezone("America/Argentina/Buenos_Aires")
       json.dumps({'x': decimal.Decimal('5.5')}, cls=DecimalEncoder)
       return json.dumps({
         'date': self.date.astimezone(arg).strftime("%d/%m/%Y %H:%M:%S"),
         'value_buy': self.value_buy,
-        'value_sell': self.value_sell}
+        'value_sell': self.value_sell,
+        'value_avg': self.value_avg}
         ,cls=DecimalEncoder)
