cbfeec60595f963045424bb6e6d37f4da4189d82
Pablo Seibelt <pabloseibelt@gmail.com>
2014-02-10 15:08:18 -0300

    Merging for commit

--- bluelytics/dolar_blue/management/commands/add_blue.py
+++ bluelytics/dolar_blue/management/commands/add_blue.py
@@ -2,18 +2,21 @@ from django.core.management.base import BaseCommand, CommandError
 from dolar_blue.models import DolarBlue
 from django.utils import timezone
 from decimal import Decimal
+import sys
 
 class Command(BaseCommand):
     args = 'valor_compra valor_venta'
     help = 'Adds the specified dollar value to the database'
 
     def handle(self, *args, **options):
-        if len(args) != 2:
+        if len(args) != 3:
             raise CommandError('Incorrect arguments')
-
+        print args
         try:
-            db = DolarBlue(date=timezone.now(), value_buy = Decimal(args[0]), value_sell=Decimal(args[1]))
+            db = DolarBlue(date=timezone.now(), value_buy = Decimal(args[0]), value_sell=Decimal(args[1]), source=args[2])
             db.save()
             self.stdout.write('Successfully saved new dollar values')
         except Exception:
             self.stdout.write('Error saving new dollar values')
+            print "Error:", sys.exc_info()[0]
+            raise
--- bluelytics/dolar_blue/models.py
+++ bluelytics/dolar_blue/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class DolarBlue(models.Model):
     value_buy = models.DecimalField(decimal_places=6, max_digits=10)
     value_sell = models.DecimalField(decimal_places=6, max_digits=10)
+    source = models.CharField(max_length=30)
     date = models.DateTimeField('fecha')
