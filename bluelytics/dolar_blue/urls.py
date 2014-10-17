367b75ea1a8b4fff3870ffec3ec561c3fe0dcbea
Pablo Seibelt <pabloseibelt@gmail.com>
2014-10-17 10:09:14 -0300

    Temporary index without menu for dns propagation

--- bluelytics/dolar_blue/urls.py
+++ bluelytics/dolar_blue/urls.py
@@ -4,6 +4,7 @@ from dolar_blue import views, api
 
 urlpatterns = patterns('',
     url(r'^json/last_price$', views.json_lastprice, name='json_lastprice'),
+    url(r'^$', views.index, name='index'),
     
     url(r'^api/last_price$', api.lastprice, name='api_lastprice'),
     url(r'^api/all_currencies$', api.all_currencies, name='api_all_currencies')
--- bluelytics/dolar_blue/views.py
+++ bluelytics/dolar_blue/views.py
@@ -14,3 +14,14 @@ def json_lastprice(request):
   max_sources = map(convDolar, maxSources())
   return HttpResponse(json.dumps(max_sources, cls=DecimalEncoder), mimetype="application/json")
 
+
+def index(request):
+  timezone.activate(pytz.timezone("America/Argentina/Buenos_Aires"))
+  max_sources = map(convDolar, maxSources())
+  max_sources_yesterday = map(convDolar, maxSourcesYesterday())
+  all_sources = map(lambda x: {"name":x.source, "description":x.description},Source.objects.all())
+  context = { 'max_sources': json.dumps(max_sources, cls=DecimalEncoder),
+              'max_sources_yesterday': json.dumps(max_sources_yesterday, cls=DecimalEncoder),
+              'all_sources': json.dumps(all_sources) }
+
+  return render(request, 'index.html', context)
