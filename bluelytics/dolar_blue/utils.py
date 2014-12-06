d1a410e832ab7d4f75eae9978b8effd7f76bac86
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2014-12-05 21:06:15 -0300

    Now allowing jsonp

--- bluelytics/dolar_blue/utils.py
+++ bluelytics/dolar_blue/utils.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 import json, pytz,decimal
 
 arg = pytz.timezone("America/Argentina/Buenos_Aires")
@@ -7,3 +8,19 @@ class DecimalEncoder(json.JSONEncoder):
         if isinstance(o, decimal.Decimal):
             return float(o)
         return super(DecimalEncoder, self).default(o)
+
+def json_response(func):
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = json.dumps(objects, cls=DecimalEncoder)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = json.dumps(str(objects), cls=DecimalEncoder)
+        return HttpResponse(data, "application/json")
+    return decorator
--- bluelytics/dolar_blue/views.py
+++ bluelytics/dolar_blue/views.py
@@ -6,13 +6,13 @@ from django.db.models import Max, Min
 import json, pytz
 
 from dolar_blue.models import DolarBlue, Source
-from dolar_blue.utils import DecimalEncoder, arg
+from dolar_blue.utils import DecimalEncoder, arg, json_response
 from dolar_blue.calculations import maxSources, maxSourcesYesterday, convDolar
 
-
+@json_response
 def json_lastprice(request):
   max_sources = map(convDolar, maxSources())
-  return HttpResponse(json.dumps(max_sources, cls=DecimalEncoder), mimetype="application/json")
+  return max_sources
 
 
 def index(request):
