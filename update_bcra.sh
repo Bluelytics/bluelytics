06b50a1028e4e339b7d605336ab20463804ce656
Pablo Seibelt <pabloseibelt@gmail.com>
2015-02-05 18:03:04 -0300

    Fix

--- update_bcra.sh
+++ update_bcra.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 . $DIR/bin/activate
-python $DIR/bluelytics/manage.py update_bcra ../bcra_scrape/out/
+python $DIR/bluelytics/manage.py update_bcra ../bcra_scraper/out/
 python $DIR/bluelytics/manage.py export_bcra bluelytics/data/graphs/bcra.json
